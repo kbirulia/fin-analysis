d160182c1c2ad6b1d8cd1902affc02931608ed08
Katsiaryna Birulia <Katsiaryna.Birulia@shell.com>
2023-11-07 14:51:15 +0300

    feat: add select ticker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import AsyncSelect from 'react-select/async';
+import axios from "axios";
 
 function App() {
+  const promiseOptions = async (inputValue) => {
+    if (!inputValue) {
+      return [];
+    }
+    console.log(inputValue);
+
+    const response = await axios({
+      method: 'get',
+      url: `/v1/finance/search?q=${inputValue}`,
+      withCredentials: false,
+    headers: {"Access-Control-Allow-Origin": "*"}
+    });
+    const options = response.data.quotes.map(data => ({value: data.symbol, label: data.symbol}));
+    return options
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AsyncSelect
+          isMulti
+          cacheOptions
+          defaultOptions
+          loadOptions={promiseOptions}
+      />
   );
 }
 
